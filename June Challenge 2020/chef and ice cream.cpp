0e5cf8577346b377a787ebca847fe8a295098313
Aditya Girish Pawate <51478802+AdityaGirishPawate@users.noreply.github.com>
2020-06-15 18:49:43 +0530

    Update chef and ice cream.cpp

--- June Challenge 2020/chef and ice cream.cpp
+++ June Challenge 2020/chef and ice cream.cpp
@@ -15,29 +15,37 @@ int32_t main() {
         currency[10]=0;
         currency[15]=0;
         bool flag= false;
+        int a[n];
+        for(int i=0;i<n;i++) cin>>a[i];
         int temp;
         for (int i = 0; i < n; ++i) {
-            cin>>temp;
-            if(temp==5) currency[5]++;
-            else if(temp==10 && currency[5]>0){
-                currency[10]++;
-                currency[5]--;
+            temp=a[i];
+            if(temp==5) 
+                currency[5]++;
+            else if(temp==10) {
+                if(currency[5]>0){
+                    currency[10]++;
+                    currency[5]--;
+                }
+                else if(currency[5]==0){
+                    flag=true;
+                    break;
+                }
             }
-            else if(temp==10 && currency[5]<=0){
-                flag=true;
-                break;
-            }
-            else if(temp==15 && currency[10]>0){
-                currency[15]++;
-                currency[10]--;
-            }
-            else if(temp==15 && currency[10]<=0 && currency[5]>1){
-                currency[15]++;
-                currency[5]-=2;
-            }
-            else if(temp==15 && currency[10]<=0 && currency[5]<=1){
-                flag=true;
-                break;
+            else{
+                
+                if(currency[10]>0){
+                    currency[15]++;
+                    currency[10]--;
+                }
+                else if(currency[10]==0 && currency[5]>= 2){
+                    currency[15]++;
+                    currency[5]-=2;
+                }
+                else if(temp==15 && currency[10]<=0 && currency[5]<=1){
+                    flag=true;
+                    break;
+                }
             }
         }
         if(flag) cout<<"NO"<<endl;
@@ -46,5 +54,3 @@ int32_t main() {
 
     return 0;
 }
-
-
