94e5c860cc9b21fd71cabf92353edbaec713b19c
Aditya Girish Pawate <51478802+AdityaGirishPawate@users.noreply.github.com>
2020-06-27 13:23:59 +0530

    Create stacks.cpp

---
+++ DSA_learning_series/Binary Search and Divide and conquer/stacks.cpp
@@ -0,0 +1,100 @@
+//const int N=1e5;
+#define INF 1e9
+#include <bits/stdc++.h>
+#include <random>
+#include <vector>
+#include <set>
+#include <map>
+#include <string>
+#include <cstdio>
+#include <cstdlib>
+#include <climits>
+#include <utility>
+#include <algorithm>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <iomanip>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+#define f(i,a,b) for(int i=a;i< b;i++)
+#define rep(i,n) f(i,0,n)
+#define fd(i,a,b) for(int i=a;i>=b;i--)
+#define pb push_back
+#define mp make_pair
+#define vi vector< int >
+#define vl vector< ll >
+#define ss second
+#define ff first
+#define pii pair< int,int >
+#define pll pair< ll,ll >
+#define sz(a) a.size()
+#define all(a) a.begin(),a.end()
+#define tri pair<int,pii>
+#define vii vector<pii>
+#define vll vector<pll>
+#define viii vector<tri>
+#define mod (1e9+7)
+#define pqueue priority_queue< int >
+#define pdqueue priority_queue< int,vi ,greater< int > >
+//#define int long long
+typedef tree<int,null_type,less<>,rb_tree_tag,tree_order_statistics_node_update> o_set;
+vector<vector<int>> g;
+//std::ios::sync_with_stdio(false);
+
+int32_t main() {
+    //std::ios::sync_with_stdio(false);
+    //cin.tie(nullptr);
+    int t;
+    cin>>t;
+    while(t--){
+        vector<int> v;
+        int n;
+        cin>>n;
+        v.clear();
+        while(n--){
+            int x;
+            cin>>x;
+            auto it=upper_bound(v.begin(),v.end(),x);
+            if(it==v.end() || v.empty()){
+                v.push_back(x);
+            }
+            else    *it=x;
+        }
+        cout<<v.size()<<" ";
+        for(auto k:v)   cout<<k<<" ";
+        cout<<endl;
+    }
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
