5aa3685c635311aacaea0df46adf9c3938a83d17
Aditya Girish Pawate <51478802+AdityaGirishPawate@users.noreply.github.com>
2020-07-13 14:11:34 +0530

    Update missing a point.cpp

--- July Long Challenge 2020/missing a point.cpp
+++ July Long Challenge 2020/missing a point.cpp
@@ -1,5 +1,5 @@
 //const int N=1e5;
-#define INF 1e9
+#define INF 2*1e18
 #include <bits/stdc++.h>
 #include <random>
 #include <vector>
@@ -40,52 +40,39 @@ using namespace __gnu_pbds;
 #define pqueue priority_queue< int >
 #define pdqueue priority_queue< int,vi ,greater< int > >
 #define int long long
-typedef tree<int,null_type,less<>,rb_tree_tag,tree_order_statistics_node_update> o_set;
+//#define int long
+typedef tree<pair<int,int>,null_type,less<>,rb_tree_tag,tree_order_statistics_node_update> o_set;
 vector<vector<int>> g;
 //std::ios::sync_with_stdio(false);
 
 int32_t main() {
-    //std::ios::sync_with_stdio(false);
-    //cin.tie(nullptr);
+    std::ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
     int t;
     cin>>t;
     while(t--){
         int n;
         cin>>n;
         vector<pair<int,int>> a(4*n-1);
-
-        set<int> s1;
-        set<int> s2;
-        int x_coord,y_coord;
+        map<int,int> mpx;
+        map<int,int> mpy;
         rep(i,4*n-1){
             int x,y;
-            cin>>x>>y;
-            a[i].first=x;
-            a[i].second=y;
-            if(*s1.find(x)==x )  s1.erase(x);
-            else s1.insert(x);
-            if(*s2.find(y)==y )  s2.erase(y);
-            else s2.insert(y);
+            cin>>a[i].first>>a[i].second;
+            mpx[a[i].first]++;
+            mpy[a[i].second]++;
         }
-        cout<<*s1.begin()<<" "<<*s2.begin()<<endl;
+        int ansx=0,ansy=0;
+        rep(i,4*n-1){
+            if(mpx[a[i].first]%2)   ansx=a[i].first;
+            if(mpy[a[i].second]%2)  ansy=a[i].second;
+            if(ansx && ansy)    break;
+        }
+        cout<<ansx<<" "<<ansy<<endl;
     }
+    return 0;
 }
-
-/*rep(i,4*n-1)
-            if(f[a[i].first]==1) x_coord=a[i].first;
-            if(s[a[i].second]==1) y_coord=a[i].second;
-            f[x]++;
-            f[x]%=2;
-            s[y]++;
-            s[y]%=2;
-        }*/
-//cout<<x_coord<<" "<<y_coord<<endl;
-//map<int,int> f;
-//map<int,int> s;
-
-
-
-
 
 
 
