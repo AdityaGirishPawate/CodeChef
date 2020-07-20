c2d7e5d837d2c4a273e026f516852f7045b8a1ee
Aditya Girish Pawate <51478802+AdityaGirishPawate@users.noreply.github.com>
2020-07-20 16:44:44 +0530

    Create appy and contest.cpp

---
+++ DSA_learning_series/Mathematics in CP/appy and contest.cpp
@@ -0,0 +1,72 @@
+//const int N=1e5;
+#define INF 1e9
+#include <bits/stdc++.h>
+#include <random>
+#include <vector>
+#include <set>
+#include <map>
+#include <string>
+#include <cstdio>
+#include <cstdlib>
+#include <climits>
+#include <utility>
+#include <algorithm>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <iomanip>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+#define f(i,a,b) for(int i=a;i< b;i++)
+#define rep(i,n) f(i,0,n)
+#define fd(i,a,b) for(int i=a;i>=b;i--)
+#define pb push_back
+#define mp make_pair
+#define vi vector< int >
+#define vl vector< ll >
+#define ss second
+#define ff first
+#define pii pair< int,int >
+#define pll pair< ll,ll >
+#define sz(a) a.size()
+#define all(a) a.begin(),a.end()
+#define tri pair<int,pii>
+#define vii vector<pii>
+#define vll vector<pll>
+#define viii vector<tri>
+#define mod (1e9+7)
+#define pqueue priority_queue< int >
+#define pdqueue priority_queue< int,vi ,greater< int > >
+#define int long long
+//#define int long
+typedef tree<int,null_type,less<>,rb_tree_tag,tree_order_statistics_node_update> o_set;
+vector<vector<int>> g;
+//std::ios::sync_with_stdio(false);
+
+int gcd(int a,int b){
+    while(b){
+        a%=b;
+        swap(a,b);
+    }
+    return a;
+}
+
+int32_t main() {
+    std::ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int t;
+    cin>>t;
+    while(t--){
+        int n,a,b,k;
+        cin>>n>>a>>b>>k;
+        int lcm=(int)((a / gcd(a,b) *b));
+        int cnt=n/a+n/b-2*(n/lcm);
+        if(cnt>=k)  cout<<"Win"<<endl;
+        else cout<<"Lose"<<endl;
+    }
+    return 0;
+}
